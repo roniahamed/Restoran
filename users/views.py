75c1b33d14a2522e356b0bf7b5071e5011c458af
roniahamed <mdroniahamed56@gmail.com>
2025-04-03 14:49:28 +0600

    Registrations

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.contrib import messages
+from django.contrib.
 
-# Create your views here.
+
+
+def CreateUser(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, Your Account is Created')
+            return redirect('/')
+    else:
+        form = UserForm()
+    return render(request, 'users/registration.html',{'form':form})
+
+
+def Login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
