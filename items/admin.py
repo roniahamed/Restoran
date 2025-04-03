db26da0c3790223e319406a793d2cb6ec6dae6cf
roniahamed <mdroniahamed56@gmail.com>
2025-04-03 20:56:35 +0600

    Item models

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Meal, Item 
 
-# Register your models here.
+admin.site.register(Category)
+
+admin.site.register(Meal)
+
+admin.site.register(Item)
--- items/models.py
+++ items/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=50, unique=True)
+
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_desc = models.CharField(max_length=200)
+    item_category = models.ManyToManyField(Meal)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    image = models.ImageField(default="item_pic.jpg", upload_to="item")
