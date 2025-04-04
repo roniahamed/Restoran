fffea36e51276f2d4d63814efcd6e3ef9ed4c6b4
roniahamed <mdroniahamed56@gmail.com>
2025-04-04 15:32:47 +0600

    update

--- items/models.py
+++ items/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.category_name
 
 class Meal(models.Model):
     meal_name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.meal_name
 
 
 class Item(models.Model):
@@ -13,3 +17,6 @@ class Item(models.Model):
     item_category = models.ManyToManyField(Meal)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     image = models.ImageField(default="item_pic.jpg", upload_to="item")
+
+    def __str__(self):
+        return self.item_name
