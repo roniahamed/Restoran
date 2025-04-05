932918c7ceef4c028462ed2ba44cc1182d761dc8
roniahamed <mdroniahamed56@gmail.com>
2025-04-05 14:52:31 +0600

    static files upload

--- Restoran/urls.py
+++ Restoran/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('items.urls')),
     path('account/', include('users.urls')),
-]+static[settings.MEDIA_URL, document_root = settings.MEDIA_ROOT]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
